b040367aff08f548a4d695d81a2f40dd29ced4da
naonmr <86028587+naonmr@users.noreply.github.com>
2022-01-20 18:37:16 +0900

    feat: ログイン関係の雛形を作成

--- front/component/shop/Header.tsx
+++ front/component/shop/Header.tsx
@@ -1,13 +1,27 @@
-import Link from "next/link";
+import { getAuth, signOut } from 'firebase/auth'
+import Link from 'next/link'
+import firebase from '../../auth/firebaseConfig'
+import PrimaryButton from '../Button'
 
 function Header() {
+  const logout = async () => {
+    const auth = getAuth(firebase)
+    try {
+      signOut(auth)
+      // history.push('/login')
+    } catch (error) {
+      alert(error)
+    }
+    console.log('ログアウト')
+  }
   return (
     <div>
       <Link href="/">
         <h1>coffee clip</h1>
       </Link>
+      <PrimaryButton text="ログアウト" onclick={logout} />
     </div>
-  );
+  )
 }
 
-export default Header;
+export default Header
---
+++ front/pages/auth/signup.tsx
@@ -0,0 +1,48 @@
+import Head from 'next/head'
+
+import Header from '../../component/shop/Header'
+import { useForm } from 'react-hook-form'
+
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+} from 'firebase/auth'
+import firebase from '../../auth/firebaseConfig'
+
+function Signup() {
+  const { register, handleSubmit } = useForm()
+  const onSubmit = async (data: any) => {
+    // firebaseする
+    console.log(data)
+    console.log(process.env.NEXT_PUBLIC_FIREBASE_APIKEY)
+
+    const auth = getAuth(firebase)
+
+    try {
+      await createUserWithEmailAndPassword(auth, data.email, data.password)
+      onAuthStateChanged(auth, (user) => {
+        // setCurrentUser(user?.uid);
+      })
+      // history.push('/login')
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  return (
+    <div>
+      <Head>
+        <title>Signup</title>
+        <meta name="Signup" content="ユーザー登録" />
+      </Head>
+      <Header />
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input {...register('email', { required: true, maxLength: 20 })} />
+        <input {...register('password')} />
+        <input type="submit" />
+      </form>
+    </div>
+  )
+}
+
+export default Signup
