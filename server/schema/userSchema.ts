6f0d2c72513158a269f0c110f809637258c40cee
Maho-Miyazawa <61673527+Maho-Miyazawa@users.noreply.github.com>
2022-01-20 18:41:17 +0900

    feat: userスキーマを作成

---
+++ server/schema/userSchema.ts
@@ -0,0 +1,35 @@
+import { Schema, Types, Model, model } from 'mongoose'
+
+interface Review {
+    image?: string
+    description?: string
+}
+
+interface User {
+    auth_id: string
+    handle_name: string
+    display_name?: string
+    icon?: string
+    follower_handle_names?: Types.Array<string>
+    followee_handle_names?: Types.Array<string>
+    reviews?: Types.DocumentArray<Review>
+}
+
+const userSchema = new Schema<User, Model<User>>({
+    auth_id: { type: String, required: true },
+    handle_name: { type: String, required: true },
+    display_name: String,
+    icon: String,
+    follower_handle_names: [String],
+    followee_handle_names: [String],
+    reviews: [
+        {
+            image: String,
+            description: String,
+        },
+    ],
+})
+
+const userModel = model<User>('User', userSchema)
+
+export default userModel
