7192b6e7b90abbb6ef81949f1fee7dc383ea604f
Maho-Miyazawa <61673527+Maho-Miyazawa@users.noreply.github.com>
2022-01-21 14:07:55 +0900

    feat: post api/usersのエンドポイントを追加

--- server/user/controller.ts
+++ server/user/controller.ts
@@ -13,3 +13,9 @@ export const getUserByHandleName = async (req: Request, res: Response) => {
     const user = data.users.filter((user) => user.handle_name === handleName)
     res.json(user[0])
 }
+
+export const postUser = async (req: Request, res: Response) => {
+    const newData = req.body
+    data.users.push(newData)
+    res.json(data)
+}
