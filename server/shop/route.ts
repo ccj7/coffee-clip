e803c380edd03f37293ee24d2318a41edb022998
Motoki-tech <>
2022-01-21 14:01:32 +0900

    add: GET文追加

--- server/shop/route.ts
+++ server/shop/route.ts
@@ -1,18 +1,49 @@
-import { Router, Request, Response } from "express";
+import { Router, Request, Response } from 'express'
+// const data = require('../dammy.json')
+// import dammy from "../dammy.json"
+
 // import {} from "./controller";
 
-const basePath = "/api/tasks";
-const routes = Router();
+// const basePath = '/api/tasks'
+const routes = Router()
 
-routes.get("/", (req: Request, res: Response) => {
-  res.json({ message: `😸 Yay!` });
-});
+const dammy = [
+    {
+        auth_id: '1',
+        handle_name: 'arasuna_coffee',
+        display_name: 'Arasuna Coffee',
+        icon: 'image',
+        address: '東京都コードクリサリス',
+        map_url: 'googlemap URL',
+        hp_url: 'HPURL',
+        instagram_url: 'instagram URL',
+        opening_hours: '9:00~10:00',
+        regular_day_off: '月曜日',
+        concept: '美味しいコーヒー提供してま〜す',
+        recommendation: {
+            title: 'グリッチ',
+            description: '酸味が特徴！',
+            image: 'image',
+        },
+        selling_point: 'image',
+        follower_auth_ids: ['ccmizki'],
+    },
+]
 
-// routes.get(basePath, getTasks);
-// routes.get(`${basePath}/:id`, getTaskById);
+routes.get('/', async (req: Request, res: Response): Promise<void> => {
+    res.json({ dammy: dammy })
+})
+
+// routes.get('/:authId', async (req: Request, res: Response): Promise<void> => {
+//     const shopAuthId = req.params.authId
+
+//     const arrayOfShop = data.filter((shop: any) => {
+//         if (shop.auth_id === shopAuthId) {
+//             res.json({ shop: shop })
+//         }
+//     })
+// })
 
 // // TODO: create more routes here
-// routes.post(`${basePath}/`, createTask);
-// routes.delete(`${basePath}/:id`, deleteTaskById);
 
-export default routes;
+export default routes
