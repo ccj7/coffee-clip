d560a1c8dc315fd9a9c3488004e37c0f2f34c32f
Maho-Miyazawa <61673527+Maho-Miyazawa@users.noreply.github.com>
2022-01-21 14:07:55 +0900

    feat: post api/usersのエンドポイントを追加

--- server/user/route.ts
+++ server/user/route.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express'
-import { getUsers, getUserByHandleName } from './controller'
+import { getUsers, getUserByHandleName, postUser } from './controller'
 
 const routes = Router()
 
 routes.get('/', getUsers)
 routes.get('/:authId', getUserByHandleName)
+
+routes.post('/', postUser)
 
 export default routes
