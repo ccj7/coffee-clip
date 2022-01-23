a593027e1c129ba7fa579b90cda6b50494f2a7bb
mei-omomo <l.3.llxl@gmail.com>
2022-01-23 23:45:31 +0900

    refactor: LogCard.tsx、ShopCard.tsxの中身を書いて、shoplist.tsxを更新

--- front/pages/user/shoplist.tsx
+++ front/pages/user/shoplist.tsx
@@ -1,31 +1,34 @@
 // TODO　U-002 github issue#23
 
 import Head from 'next/head'
-import Link from 'next/link'
+import ShopCard from '../../components/user/ShopCard'
 
 import UserHeader from '../../components/user/UserHeader'
 
 function Shoplist() {
   const shoplist = [
     {
-      name: 'Arasuna Coffee',
-      shopId: 'arasuna_coffee',
+      display_name: 'Arasuna Coffee',
+      handle_name: '@arasuna_kiyosumi',
       image: 'ここにImg',
-      comment: '美味しいコーヒー提供してま〜す',
+      description: '美味しいコーヒー提供してま〜す',
+      url: '/',
     },
     {
-      name: 'Arasuna Coffee2',
-      shopId: 'Arasuna_kiyosumi',
+      display_name: 'Arasuna Coffee2',
+      handle_name: '@arasuna_kiyosumi',
       image: 'ここにImg',
-      comment:
-        '清澄白河にあるカフェです。清澄白河にあるカフェです。清澄白河にあるカフェです。',
+      description:
+        '清澄白河にあるカフェです。清澄白河にあるカフェです。清澄白河にあるカフェです。清澄白河にあるカフェです。清澄白河にあるカフェです。清澄白河にあるカフェです。',
+      url: '/',
     },
     {
-      name: 'Arasuna Coffee3',
-      shopId: 'Arasuna_kiyosumi',
+      display_name: 'Arasuna Coffee3',
+      handle_name: '@arasuna_kiyosumi',
       image: 'ここにImg',
-      comment:
+      description:
         '清澄白河にあるカフェです。清澄白河にあるカフェです。清澄白河にあるカフェです。',
+      url: '/',
     },
   ]
 
@@ -36,20 +39,19 @@ function Shoplist() {
         <meta name="shoplist" content="ショップ一覧" />
       </Head>
       <UserHeader />
-      <section>
-        {shoplist.map((shop) => {
-          return (
-            <Link href="/">
-              {/* TODO リンク先は各shopのTOPページ */}
-              <div key={shop.shopId}>
-                <p>{shop.name}</p>
-                <p>{shop.shopId}</p>
-                <p>{shop.image}</p>
-                <p>{shop.comment}</p>
-              </div>
-            </Link>
-          )
-        })}
+      <section className='flex flex-wrap'>
+      { shoplist.map((shop, key) => {
+        return (
+        <ShopCard
+         key={key}
+         display_name={shop.display_name}
+         handle_name={shop.handle_name}
+         image={shop.image}
+         description={shop.description}
+         url={shop.url}
+         />
+        )})
+      }
       </section>
     </div>
   )
