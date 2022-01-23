ae692eb23f9a906ebc1a64ec119f7c9d0240b1e9
Maho Miyata <k.paddy123+github@gmail.com>
2022-01-24 00:40:41 +0900

    feat: MongoDBも追加&接続（おまけでmongo-expressも入れた）

--- server/app.ts
+++ server/app.ts
@@ -2,6 +2,13 @@ import express from "express";
 import shopRoute from "./shop/route";
 import userRoute from "./user/route";
 
+// TODO: デプロイ時の接続確認用に読み込んでいます。あとで削除すること！
+import kittySchema from "./schema/sampleSchema";
+import connectToDB from "./db-connection";
+import mongoose from "mongoose";
+import { Request, Response } from 'express'
+// ここまで削除
+
 const app: express.Express = express();
 app.use(express.json());
 
@@ -17,5 +24,21 @@ app.use(express.json());
 
 app.use("/api/shops", shopRoute);
 app.use("/api/users", userRoute);
+
+// TODO: デプロイ時の接続確認用のテストパスです。あとで削除すること！
+app.get("/api/test", async (req, res) => {
+  await connectToDB()
+  const data = await mongoose.model('Kitten', kittySchema).find();
+  res.send(data);
+})
+app.post("/api/test", async (req: Request, res: Response) => {
+  const data = req.body;
+  const Kitten = mongoose.model('Kitten', kittySchema);
+  const newKitten = new Kitten(data);
+  await connectToDB()
+  await newKitten.save()
+  res.send("created!")
+})
+// テストパスここまで
 
 export default app;
