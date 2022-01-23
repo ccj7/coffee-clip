799a5fbec8cae6c6c78d78ef7a1617497c6638fb
Maho Miyata <k.paddy123+github@gmail.com>
2022-01-23 15:40:22 +0900

    fix: サインアップ時のエラー修正

--- front/pages/auth/signup.tsx
+++ front/pages/auth/signup.tsx
@@ -2,31 +2,35 @@ import Head from 'next/head'
 
 import Header from '../../components/shop/Header'
 import { useForm } from 'react-hook-form'
+import { useRouter } from 'next/router'
 
 import {
   getAuth,
   createUserWithEmailAndPassword,
-  onAuthStateChanged,
 } from 'firebase/auth'
 import firebase from '../../auth/firebaseConfig'
 
 function Signup() {
+
   const { register, handleSubmit } = useForm()
+  const router = useRouter()
+
   const onSubmit = async (data: any) => {
-    // firebaseする
     console.log(data)
     console.log(process.env.NEXT_PUBLIC_FIREBASE_APIKEY)
-
     const auth = getAuth(firebase)
 
     try {
-      await createUserWithEmailAndPassword(auth, data.email, data.password)
-      onAuthStateChanged(auth, (user) => {
-        // setCurrentUser(user?.uid);
-      })
-      // history.push('/login')
-    } catch (error) {
-      console.log(error)
+      createUserWithEmailAndPassword(auth, data.email, data.password)
+        .then((userCredential) => {
+          const user = userCredential.user;
+          console.log(user)
+          router.push('/user/mypage')
+        })
+    } catch (error: any) {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(errorCode, errorMessage)
     }
   }
   return (
@@ -37,7 +41,7 @@ function Signup() {
       </Head>
       <Header />
       <form onSubmit={handleSubmit(onSubmit)}>
-        <input {...register('email', { required: true, maxLength: 20 })} />
+        <input {...register('email')} />
         <input {...register('password')} />
         <input type="submit" />
       </form>
