78a486c6350c72b9f7bde70665149a4920749ecc
Maho-Miyazawa <61673527+Maho-Miyazawa@users.noreply.github.com>
2022-01-23 23:04:52 +0900

    fix: getとpost先をjsonファイルからMongoDBに修正

--- server/user/controller.ts
+++ server/user/controller.ts
@@ -1,21 +1,42 @@
 import { Request, Response } from 'express'
+import { connect } from 'mongoose'
+import userModel from '../schema/userSchema'
 
-// TODO jsonファイルからMongoDBに変更
-import data from './users.json'
+const dbConnect = () => connect('mongodb://localhost:27017/coffee_clip')
 
-export const getUsers = async (req: Request, res: Response) => {
-    res.json(data)
+export const getUsers = async (req: Request, res: Response): Promise<void> => {
+    await dbConnect()
+    const users = await userModel.find({})
+    res.json(users)
 }
 
-export const getUserByAuthId = async (req: Request, res: Response) => {
+export const getUserByAuthId = async (
+    req: Request,
+    res: Response
+): Promise<void> => {
+    await dbConnect()
     const authId: String = req.params.authId
+    const user = await userModel.findOne({ auth_id: authId })
 
-    const user = data.users.filter((user) => user.auth_id === authId)
-    res.json(user[0])
+    res.json(user)
 }
 
-export const postUser = async (req: Request, res: Response) => {
+export const postUser = async (req: Request, res: Response): Promise<void> => {
+    await dbConnect()
     const newData = req.body
-    data.users.push(newData)
-    res.json(data)
+
+    const authIdCheck = await userModel.findOne({
+        auth_id: newData.auth_id,
+    })
+
+    const handleNameCheck = await userModel.findOne({
+        handle_name: newData.handle_name,
+    })
+
+    if (!authIdCheck && !handleNameCheck) {
+        await userModel.create(newData)
+        res.json(newData)
+    } else {
+        res.status(400).send('auth_id or handle_nameが被っています')
+    }
 }
