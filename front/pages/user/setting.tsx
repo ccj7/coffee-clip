f1356f38afd83acc6c944a35c8bd640b2c9f9e7a
naonmr <86028587+naonmr@users.noreply.github.com>
2022-01-24 11:20:26 +0900

    inin: セッティングページの雛形のみ作成

---
+++ front/pages/user/setting.tsx
@@ -0,0 +1,19 @@
+// TODO　U-001 github issue#20
+
+import Head from 'next/head'
+
+import Header from '../../components/shop/Header'
+
+function Setting() {
+  return (
+    <div>
+      <Head>
+        <title>Setting</title>
+        <meta name="Setting" content="Setting" />
+      </Head>
+      <Header />
+    </div>
+  )
+}
+
+export default Setting
