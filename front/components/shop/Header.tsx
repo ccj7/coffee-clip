fac854983073903999b27d6d7112e1130c9858cb
naonmr <86028587+naonmr@users.noreply.github.com>
2022-01-24 10:47:51 +0900

    init: チャクラiconをインストール

--- front/components/shop/Header.tsx
+++ front/components/shop/Header.tsx
@@ -1,7 +1,18 @@
+import {
+  HStack,
+  IconButton,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+} from '@chakra-ui/react'
+import { HamburgerIcon, NotAllowedIcon } from '@chakra-ui/icons'
+
 import { getAuth, signOut } from 'firebase/auth'
 import Link from 'next/link'
 import firebase from '../../auth/firebaseConfig'
 import PrimaryButton from '../Button'
+import { Text } from '@chakra-ui/react'
 
 function Header() {
   const logout = async () => {
@@ -16,6 +27,25 @@ function Header() {
   }
   return (
     <div>
+      <HStack>
+        <Link href="/">
+          <Text fontSize="3xl">COFFEE CLIP</Text>
+        </Link>
+        <Menu>
+          <MenuButton
+            as={IconButton}
+            aria-label="Options"
+            icon={<HamburgerIcon />}
+            variant="outline"
+            color="brand.300"
+          />
+          <MenuList>
+            <MenuItem icon={<NotAllowedIcon />} onClick={logout}>
+              Logout
+            </MenuItem>
+          </MenuList>
+        </Menu>
+      </HStack>
       <Link href="/">
         <h1>coffee clip</h1>
       </Link>
