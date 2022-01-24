a00dd033d7055561b3f742738b796b6dc0076297
Motoki-tech <>
2022-01-24 16:47:46 +0900

    feat:【GET】shopダッシュボード編集画面

--- server/shop/route.ts
+++ server/shop/route.ts
@@ -2,7 +2,7 @@ import { Router, Request, Response } from 'express'
 import connectToDB from '../db-connection'
 import ShopsDataModel from '../schema/shopSchema'
 // import { shopDashboard } from './controller'
-import { getShops, getShop } from './controller'
+import { getShops, getShop, getShopEdit } from './controller'
 
 const routes = Router()
 
@@ -11,12 +11,8 @@ routes.get('/', getShops)
 //shopダッシュボード画面
 routes.get('/:authId', getShop)
 
-//shopダッシュボード編集画面
-// routes.get('/:authId/edit', async (req: Request, res: Response) => {
-//     await connectToDB()
-//     const data = await ShopsDataModel.findOne({ auth_id: req.params.authId })
-//     res.json(data)
-// })
+// shopダッシュボード編集画面
+routes.get('/:authId/edit', getShopEdit)
 
 // routes.put('/:authId/edit', async (req: Request, res: Response) => {
 //     await connectToDB()
