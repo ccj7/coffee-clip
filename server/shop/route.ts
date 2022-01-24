290dc0758c2669c0a0dabfcf0baf48c38d5a694c
Motoki-tech <>
2022-01-24 16:14:21 +0900

    add:shopのダッシュボード画面エンドポイント

--- server/shop/route.ts
+++ server/shop/route.ts
@@ -1,13 +1,34 @@
 import { Router, Request, Response } from 'express'
-// const data = require('../dammy.json')
-// import dammy from "../dammy.json"
+import connectToDB from '../db-connection'
+import ShopsDataModel from '../schema/shopSchema'
+// import { shopDashboard } from './controller'
 
-import { getShops } from "./controller";
-
-// const basePath = '/api/tasks'
 const routes = Router()
 
-routes.get('/', getShops)
+//shopダッシュボード画面
+routes.get('/:authId', async (req: Request, res: Response) => {
+    await connectToDB()
+    const data = await ShopsDataModel.findOne({ auth_id: req.params.authId })
+    res.json(data)
+})
+
+// //フォローしているリスト
+// routes.put('/:authId/following', async (req: Request, res: Response) => {
+//     await connectToDB()
+//     const data = await ShopsDataModel.find()
+//     res.json(data)
+
+// //
+// routes.put('/list', async (req: Request, res: Response) => {
+//     await connectToDB()
+//     const data = await ShopsDataModel.find()
+//     res.send(data)
+
+// //shopダッシュボード編集画面
+// routes.put('/:authId/edit', async (req: Request, res: Response) => {
+//     await connectToDB()
+//     const data = await ShopsDataModel.find()
+//     res.send(data)
 
 // routes.get('/:authId', async (req: Request, res: Response): Promise<void> => {
 //     const shopAuthId = req.params.authId
