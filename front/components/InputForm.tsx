d80092eb1180a47cba7fbea47fb939f43a659192
naonmr <86028587+naonmr@users.noreply.github.com>
2022-01-24 16:17:06 +0900

    feat: ショップ情報修正フォーム作成

--- front/components/InputForm.tsx
+++ front/components/InputForm.tsx
@@ -5,30 +5,33 @@ import {
   Input,
   Button,
 } from '@chakra-ui/react'
-import { useForm } from 'react-hook-form'
+import { useForm, useFormContext } from 'react-hook-form'
 
 function InputForm(props: any) {
   const { thema, text, defaultValue } = props
 
+  // const {
+  //   register,
+  //   formState: { errors, isSubmitting },
+  // } = useForm()
+  const methods = useFormContext()
   const {
-    register,
-    formState: { errors, isSubmitting },
-  } = useForm()
+    formState: { errors },
+  } = useFormContext()
 
   return (
     <>
-      <FormControl isInvalid={errors.thema}>
+      <FormControl isInvalid={errors[thema]}>
         <FormLabel htmlFor={thema}>{text}</FormLabel>
         <Input
           id={thema}
           defaultValue={defaultValue}
-          {...register(thema, {
-            required: 'This is required',
-            // minLength: { value: 4, message: 'Minimum length should be 4' },
+          {...methods.register(thema, {
+            required: true,
           })}
         />
         <FormErrorMessage>
-          {errors.thema && errors.thema.message}
+          {errors[thema] && 'This is required'}
         </FormErrorMessage>
       </FormControl>
     </>
