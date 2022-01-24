428002cc04f41e830550386b662fbf9d3460d5df
mei-omomo <l.3.llxl@gmail.com>
2022-01-24 18:30:54 +0900

    feat: エンドポイントからgetするようにshoplist.tsxを修正

--- front/components/user/ShopCard.tsx
+++ front/components/user/ShopCard.tsx
@@ -3,14 +3,15 @@ import { Box, Heading, Image, Text } from '@chakra-ui/react'
 
 // TODO propsの型を変換する
 function ShopCard(props: any) {
-  const { display_name, handle_name, image, description, url } = props;
+  const { auth_id, display_name, handle_name, icon, concept } = props;
   return (
-    <Link href={url}>
-      <Box w={600} >
+    // TODO: auth_idを使ったLink先
+    <Link href="/">
+      <Box w={600} m={10}>
         <Heading size="lg">{display_name}</Heading>
         <Text>{handle_name}</Text>
-        <Image src={image} />
-        <Text>{description}</Text>
+        <Image src={icon} />
+        <Text>{concept}</Text>
       </Box>
     </Link>
   )
