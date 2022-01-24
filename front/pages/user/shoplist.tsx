d40b151fb496298c7e3b38ac61ffed9151a4129c
mei-omomo <l.3.llxl@gmail.com>
2022-01-24 18:30:54 +0900

    feat: エンドポイントからgetするようにshoplist.tsxを修正

--- front/pages/user/shoplist.tsx
+++ front/pages/user/shoplist.tsx
@@ -1,54 +1,54 @@
 // TODO　U-002 github issue#23
 
 import Head from 'next/head'
+import axios from 'axios'
+
 import ShopCard from '../../components/user/ShopCard'
 import UserHeader from '../../components/user/UserHeader'
 import { Box, Flex } from '@chakra-ui/react'
+import { useEffect, useState } from 'react'
+
+interface Shop {
+  auth_id: string,
+  display_name: string,
+  handle_name: string,
+  icon?: string,
+  selling_point?: string
+}
+
 
 function Shoplist() {
-  const shoplist = [
-    {
-      display_name: 'Arasuna Coffee',
-      handle_name: '@arasuna_kiyosumi',
-      image: 'https://picsum.photos/300/200/?random',
-      description: '美味しいコーヒー提供してま〜す',
-      url: '/',
-    },
-    {
-      display_name: 'Arasuna Coffee2',
-      handle_name: '@arasuna_kiyosumi',
-      image: 'https://picsum.photos/300/200/?random',
-      description:
-        '清澄白河にあるカフェです。清澄白河にあるカフェです。清澄白河にあるカフェです。清澄白河にあるカフェです。清澄白河にあるカフェです。清澄白河にあるカフェです。',
-      url: '/',
-    },
-    {
-      display_name: 'Arasuna Coffee3',
-      handle_name: '@arasuna_kiyosumi',
-      image: 'https://picsum.photos/300/200/?random',
-      description:
-        '清澄白河にあるカフェです。清澄白河にあるカフェです。清澄白河にあるカフェです。',
-      url: '/',
-    },
-  ];
+  const [shopsInfo, setShopsInfo] = useState<[Shop]>();
+
+  // shopの一覧情報を取得
+  useEffect(() => {
+    const getShopsInfo = async () => {
+      const res: any = await axios.get('/api/shops');
+      // 本番はdammyはいらない
+      setShopsInfo(res.data.dammy)
+    }
+
+    getShopsInfo()
+  }, []) 
 
   return (
     <Box>
       <Head>
         <title>ショップ一覧</title>
-        <meta name="shoplist" content="ショップ一覧" />
+        <meta name="shopslist" content="ショップ一覧" />
       </Head>
       <UserHeader />
       <Flex>
-        { shoplist.map((shop, key) => {
+        { shopsInfo &&
+         shopsInfo.map((shop: any, key: any) => {
           return (
             <ShopCard
             key={key}
+            auth_id={shop.auth_id}
             display_name={shop.display_name}
             handle_name={shop.handle_name}
-            image={shop.image}
-            description={shop.description}
-            url={shop.url} />
+            icon={shop.icon}
+            concept={shop.concept} />
           )})
         }
       </Flex>
