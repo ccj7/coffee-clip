b7258eef0d2bf098d6fad6eb4ecd7b3e51f79ce9
Motoki-tech <motokimizuno.0107@gmail.com>
2022-01-24 16:47:46 +0900

    feat:【GET】shopダッシュボード編集画面

--- server/shop/controller.ts
+++ server/shop/controller.ts
@@ -13,3 +13,9 @@ export const getShop = async (req: Request, res: Response) => {
     const data = await ShopsDataModel.findOne({ auth_id: req.params.authId })
     res.json(data)
 }
+
+export const getShopEdit = async (req: Request, res: Response) => {
+    await connectToDB()
+    const data = await ShopsDataModel.findOne({ auth_id: req.params.authId })
+    res.json(data)
+}
