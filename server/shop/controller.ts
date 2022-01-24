ecfd11c60d59c739999bee0ed1fffe77b438e90e
Motoki-tech <>
2022-01-24 16:37:52 +0900

    feat:書き方修正

--- server/shop/controller.ts
+++ server/shop/controller.ts
@@ -3,7 +3,13 @@ import connectToDB from '../db-connection'
 import ShopsDataModel from '../schema/shopSchema'
 
 export const getShops = async (req: Request, res: Response) => {
-  await connectToDB()
-  const data = await ShopsDataModel.find()
-  res.send(data)
+    await connectToDB()
+    const data = await ShopsDataModel.find()
+    res.send(data)
+}
+
+export const getShop = async (req: Request, res: Response) => {
+    await connectToDB()
+    const data = await ShopsDataModel.findOne({ auth_id: req.params.authId })
+    res.json(data)
 }
