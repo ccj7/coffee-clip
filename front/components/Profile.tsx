987d7fdc26a6859c1aacced24da035b310884f4d
naonmr <86028587+naonmr@users.noreply.github.com>
2022-01-25 14:47:22 +0900

    feat: アイコン画像を表示

--- front/components/Profile.tsx
+++ front/components/Profile.tsx
@@ -1,13 +1,13 @@
 import Image from './Image'
-import { Stack, HStack, Heading, Box } from '@chakra-ui/react'
+import { Avatar, Stack, HStack, Heading, Box } from '@chakra-ui/react'
 
 // TODO propsの型を変換する
 function PhotoImage(props: any) {
-  const { display_name, handle_name } = props
+  const { display_name, handle_name, icon } = props
   return (
     <>
       <HStack>
-        <Image />
+        <Avatar size="lg" src={icon} alt="icon" />{' '}
         <Stack>
           <Heading size="lg">{display_name}</Heading>
           <Box>@{handle_name}</Box>
