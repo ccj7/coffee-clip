422d178fc5da1a26f8b485892f11a4723c39b768
Motoki-tech <motokimizuno.0107@gmail.com>
2022-01-25 10:40:46 +0900

    修正

--- server/shop/route.ts
+++ server/shop/route.ts
@@ -12,14 +12,14 @@ routes.get('/', getShops)
 routes.get('/:authId', getShop)
 
 // 【GET】shopダッシュボード編集画面
-routes.get('/:authId/edit', getShopEdit)
+// routes.get('/:authId/edit', getShopEdit)
 
 // 【PUT】shopダッシュボード編集画面
-// routes.put('/:authId/edit', async (req: Request, res: Response) => {
+// routes.put('/:authId', async (req: Request, res: Response) => {
 //     await connectToDB()
 //     const data = await ShopsDataModel.findOne({ auth_id: req.params.authId })
 //     const body = {
-//         auth_id: { type: req., required: true },
+//         auth_id: data?.auth_id.valueOf,
 //         handle_name: { type: , required: true },
 //         display_name: { type: , required: true },
 //         icon: ,
