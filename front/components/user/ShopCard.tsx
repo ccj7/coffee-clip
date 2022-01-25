a04eec01bad8d2c0d557b75f751a51f163973874
mei-omomo <l.3.llxl@gmail.com>
2022-01-25 12:14:21 +0900

    feat: shopのページに飛べるように変更

--- front/components/user/ShopCard.tsx
+++ front/components/user/ShopCard.tsx
@@ -6,7 +6,7 @@ function ShopCard(props: any) {
   const { display_name, handle_name, icon, concept } = props;
   return (
     // TODO: 本番はhandle_nameを使ったLink先
-    <Link href="/shop/shop-top-page">
+    <Link href={`/shop/top-page/${handle_name}`}>
       <Box w={600} m={10} border='2px' borderColor='gray.200' textAlign='center'>
         <Heading size="lg">{display_name}</Heading>
         <Text>{handle_name}</Text>
