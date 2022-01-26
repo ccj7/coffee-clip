4d7901105c2adef057d3330de573c028fac211d7
mei-omomo <l.3.llxl@gmail.com>
2022-01-26 11:55:48 +0900

    feat: 送るデータをconsole.logに表示

--- front/pages/user/setting.tsx
+++ front/pages/user/setting.tsx
@@ -1,19 +1,36 @@
 // TODO　U-003 github issue#21
 
 import Head from 'next/head'
-// import axios from 'axios'
+import axios from 'axios'
 
 import UserHeader from '../../components/user/UserHeader'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useForm, FormProvider } from 'react-hook-form' 
-import { Box, Button, Heading } from '@chakra-ui/react'
+import { Box, Button, Heading, Image } from '@chakra-ui/react'
 import InputForm from '../../components/InputForm'
 
 // TODO: 全体的に型をちゃんと定義する
-function Setting() { 
+function Setting() {
+  
+  // user情報を取得
+  useEffect(() => {
+    const getUser = async (authId: string) => {
+      const res: any = await axios.get(`/api/users/${authId}`)
+      setDisplayName(res.data.display_name)
+    }
 
-  const [image, setImage] = useState("");
-  const [base64Code, setBase64Code] = useState("");
+    // TODO: 実際の値に変更
+    getUser("4E5Jby73IVRAypSDyV3IfFcQwXz1")
+  }, [])
+
+  // アップロードされた画像のデータ
+  const [image, setImage] = useState("")
+  // base64に変換したもの
+  const [base64Code, setBase64Code] = useState("")
+  // getしてきたuser情報を入れておく予定
+  const [displayName, setDisplayName] = useState<any>()
+
+  const methods = useForm()
 
   // アップされた画像データをimageにセット
   const handleChangeImage = (e: any) => {
@@ -28,36 +45,27 @@ function Setting() {
       const base64 = reader.result as string
       console.log(base64)
 
-      // この時点でセットされるようにする
       setBase64Code(base64)
     }
   }
 
-  // アップロードする関数
+  // 画像をアップロードする関数
   const uploadImage = () => {
     encodeToBase64(image)
-    
-    // ここでbase64Codeを使ってfetchする
   }
 
-
-  // 仮データ
-  const dammy = {
-    auth_id: '1',
-    display_name: 'Kaori'
-  }
-  const [userInfo, setUserInfo] = useState<any>(dammy)
-  const methods = useForm()
-
-  const onSubmit = (data: any) => {
-    console.log(data)
+  // サーバーに送る
+  const onSubmit = (changeUserInfo: any) => {
+    changeUserInfo.base64 = base64Code
+    // TODO: changeUserInfoを送る
+    console.log(changeUserInfo)
   }
 
   return (
     <Box>
       <Head>
         <title>Setting</title>
-        <meta name="Setting" content="Setting" />
+        <meta name="Setting" content="ユーザープロフィール編集" />
       </Head>
       <UserHeader />
       <Heading size='md' m={'16px'}>ユーザープロフィール</Heading>
@@ -66,11 +74,13 @@ function Setting() {
           <InputForm
            thema="display_name"
            text="ユーザーネーム"
-           defaultValue={userInfo.display_name}
+           defaultValue={displayName}
            />
-          <input type="file" accept="image/*" onChange={handleChangeImage}/>
+          <input type="file" accept="image/png, image/jpeg" onChange={handleChangeImage}/>
           <Button type="submit" onClick={uploadImage}>アップロード</Button>
-
+          { base64Code &&
+            <Image w={"300px"} src={base64Code} />
+          }
           <Box><Button mt={4} type="submit">保存</Button></Box>
         </form>
       </FormProvider>
