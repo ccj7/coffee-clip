ad3b0d80371762d01997e5136d66742fb34055a7
Maho Miyata <k.paddy123+github@gmail.com>
2022-01-27 11:27:33 +0900

    fix: DBから探すときの条件指定

--- server/shop/controller.ts
+++ server/shop/controller.ts
@@ -4,9 +4,8 @@ import ShopsDataModel from '../schema/shopSchema'
 
 export const getShops = async (req: Request, res: Response) => {
     await connectToDB()
-    const data = await ShopsDataModel.find()
-    const publishedShops = data.filter(shop => shop.publish_state);
-    res.send(publishedShops)
+    const data = await ShopsDataModel.find({publish_state: true})
+    res.send(data)
 }
 
 export const getShop = async (req: Request, res: Response) => {
