a1d75deb18f9fc2daf4c31bd33efa1d81e92f0a6
Maho Miyata <k.paddy123+github@gmail.com>
2022-01-27 10:43:43 +0900

    chore: ルート整理

--- server/shop/route.ts
+++ server/shop/route.ts
@@ -1,7 +1,4 @@
-import { Router, Request, Response } from 'express'
-import connectToDB from '../db-connection'
-import ShopsDataModel from '../schema/shopSchema'
-// import { shopDashboard } from './controller'
+import { Router } from 'express'
 import { getShops, getShop, putShop, postShop } from './controller'
 
 const routes = Router()
@@ -17,28 +14,5 @@ routes.put('/:authId', putShop)
 routes.get('/details/:handleName', getShop)
 
 routes.post('/', postShop)
-// //フォローしているリスト
-// routes.put('/:authId/following', async (req: Request, res: Response) => {
-//     await connectToDB()
-//     const data = await ShopsDataModel.find()
-//     res.json(data)
-
-// //
-// routes.put('/list', async (req: Request, res: Response) => {
-//     await connectToDB()
-//     const data = await ShopsDataModel.find()
-//     res.send(data)
-
-// routes.get('/:authId', async (req: Request, res: Response): Promise<void> => {
-//     const shopAuthId = req.params.authId
-
-//     const arrayOfShop = data.filter((shop: any) => {
-//         if (shop.auth_id === shopAuthId) {
-//             res.json({ shop: shop })
-//         }
-//     })
-// })
-
-// // TODO: create more routes here
 
 export default routes
