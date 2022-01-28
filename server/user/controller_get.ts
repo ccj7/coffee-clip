d7b56c1210e1051bc99fed9513b334983dfbcb89
Maho-Miyazawa <61673527+Maho-Miyazawa@users.noreply.github.com>
2022-01-28 14:00:15 +0900

    fix: GET フォローshop時にtry-catchとエラーメッセージ追加

--- server/user/controller_get.ts
+++ server/user/controller_get.ts
@@ -170,33 +170,40 @@ export const getFolloweeShops = async (
     req: Request,
     res: Response
 ): Promise<void> => {
-    await connectToDB()
-    const authId: String = req.params.authId
-    const user = await userModel.findOne({ auth_id: authId })
+    try {
+        await connectToDB()
+        const authId: String = req.params.authId
+        const user = await userModel.findOne({ auth_id: authId })
 
-    const followeeShops = []
+        const followeeShops = []
 
-    if (user && user.followee_shops_handle_names) {
-        for (const shopHandleName of user.followee_shops_handle_names) {
-            const shop = await ShopsDataModel.findOne(
-                {
-                    handle_name: shopHandleName,
-                    publish_state: true,
-                },
-                {
-                    handle_name: 1,
-                    display_name: 1,
-                    icon: 1,
-                    concept: 1,
-                    _id: 0,
+        if (user) {
+            if (user.followee_shops_handle_names) {
+                for (const shopHandleName of user.followee_shops_handle_names) {
+                    const shop = await ShopsDataModel.findOne(
+                        {
+                            handle_name: shopHandleName,
+                            publish_state: true,
+                        },
+                        {
+                            handle_name: 1,
+                            display_name: 1,
+                            icon: 1,
+                            concept: 1,
+                            _id: 0,
+                        }
+                    )
+
+                    if (shop) {
+                        followeeShops.push(shop)
+                    }
                 }
-            )
-
-            if (shop) {
-                followeeShops.push(shop)
             }
+            res.json({ followeeShops: followeeShops })
+        } else {
+            res.status(400).json({ error: 'userが見つかりません' })
         }
+    } catch (err) {
+        res.status(400).send(err)
     }
-
-    res.json({ followeeShops: followeeShops })
 }
