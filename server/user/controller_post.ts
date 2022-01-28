dc69ad2e940e2363d0de001b5e4cda650ff17ab0
Maho-Miyazawa <61673527+Maho-Miyazawa@users.noreply.github.com>
2022-01-28 15:01:09 +0900

    fix: POST userにtry-catchとエラーメッセージを追加

--- server/user/controller_post.ts
+++ server/user/controller_post.ts
@@ -4,52 +4,64 @@ import { s3Upload } from '../s3'
 import userModel from '../schema/userSchema'
 
 export const postUser = async (req: Request, res: Response): Promise<void> => {
-    await connectToDB()
-    const bodyData = req.body
+    try {
+        await connectToDB()
+        const bodyData = req.body
 
-    const authIdCheck = await userModel.findOne({
-        auth_id: bodyData.auth_id,
-    })
-
-    const handleNameCheck = await userModel.findOne({
-        handle_name: bodyData.handle_name,
-    })
-
-    if (!authIdCheck && !handleNameCheck) {
-        let iconData: String = ''
-
-        if (bodyData.icon) {
-            const imgFileName: string = `icon_user_${bodyData.auth_id}`
-            s3Upload(bodyData.icon, imgFileName)
-            iconData = await s3Upload(bodyData.icon, imgFileName)
-        }
-
-        interface User {
-            auth_id: String
-            handle_name: String
-            display_name: String
-            icon: String | undefined
-            follower_handle_names: Array<String>
-            followee_handle_names: Array<String>
-            followee_shops_handle_names: Array<String>
-            reviews: Array<String>
-        }
-
-        const newUser: User = {
+        const authIdCheck = await userModel.findOne({
             auth_id: bodyData.auth_id,
-            handle_name: bodyData.handle_name,
-            display_name: bodyData.display_name,
-            icon: iconData,
-            follower_handle_names: [],
-            followee_handle_names: [],
-            followee_shops_handle_names: [],
-            reviews: [],
-        }
+        })
 
-        await userModel.create(newUser)
-        res.status(200).end()
-    } else {
-        res.status(400).end()
+        const handleNameCheck = await userModel.findOne({
+            handle_name: bodyData.handle_name,
+        })
+
+        if (!authIdCheck && !handleNameCheck) {
+            let iconData: String = ''
+
+            if (bodyData.icon) {
+                const imgFileName: string = `icon_user_${bodyData.auth_id}`
+                s3Upload(bodyData.icon, imgFileName)
+                iconData = await s3Upload(bodyData.icon, imgFileName)
+            }
+
+            interface User {
+                auth_id: String
+                handle_name: String
+                display_name: String
+                icon: String | undefined
+                follower_handle_names: Array<String>
+                followee_handle_names: Array<String>
+                followee_shops_handle_names: Array<String>
+                reviews: Array<String>
+            }
+
+            const newUser: User = {
+                auth_id: bodyData.auth_id,
+                handle_name: bodyData.handle_name,
+                display_name: bodyData.display_name,
+                icon: iconData,
+                follower_handle_names: [],
+                followee_handle_names: [],
+                followee_shops_handle_names: [],
+                reviews: [],
+            }
+
+            await userModel.create(newUser)
+            res.status(200).json({ message: '新規ユーザーを作成しました' })
+        } else if (authIdCheck) {
+            res.status(400).json({
+                error: '既に同じ認証IDが存在するため無効です',
+            })
+        } else if (authIdCheck) {
+            res.status(400).json({
+                error: '既に同じユーザーIDが存在するため無効です',
+            })
+        } else {
+            res.status(400).json({ error: 'エラーです' })
+        }
+    } catch (err) {
+        res.status(400).send(err)
     }
 }
 
