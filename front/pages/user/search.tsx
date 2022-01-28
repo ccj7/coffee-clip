ce69a59cbe756a4a6b90855a61e951aa324f978e
naonmr <86028587+naonmr@users.noreply.github.com>
2022-01-28 11:23:55 +0900

    feat: shopの検索結果表示

--- front/pages/user/search.tsx
+++ front/pages/user/search.tsx
@@ -1,27 +1,89 @@
 import Head from 'next/head'
-import { VFC } from 'react'
+import { useEffect, useState, VFC } from 'react'
 
-import Header from '../../components/shop/Header'
 import { useAuthContext } from '../../auth/AuthContext'
 import { isLoggedIn } from '../../util'
 import UserHeader from '../../components/user/UserHeader'
 import { useRouter } from 'next/router'
+import {
+  Box,
+  Heading,
+  Tabs,
+  TabList,
+  TabPanels,
+  Tab,
+  TabPanel,
+  Text,
+} from '@chakra-ui/react'
+import { async } from '@firebase/util'
+import axios from 'axios'
+import ShopCard from '../../components/user/ShopCard'
+
+interface Shop {
+  auth_id: string
+  display_name: string
+  handle_name: string
+  icon?: string
+  selling_point?: string
+}
 
 const Search: WithGetAccessControl<VFC> = (props) => {
   const { currentUser } = useAuthContext()
   const router = useRouter()
-
   const keyword = router.query.about
 
-  console.log(keyword)
+  const [shopsInfo, setShopsInfo] = useState<Shop[]>([])
+
+  useEffect(() => {
+    const getSearchResult = async () => {
+      const res = await axios.get(`/api/users/search?keyword=${keyword}`)
+      console.log(res.data.users)
+      setShopsInfo(res.data.shops)
+    }
+    getSearchResult()
+  }, [keyword])
+
   return (
-    <div>
+    <Box>
       <Head>
         <title>検索結果</title>
         <meta name="Search" content="Search" />
       </Head>
       <UserHeader />
-    </div>
+      <Heading>検索結果</Heading>
+
+      <Tabs>
+        <TabList>
+          <Tab>Users</Tab>
+          <Tab>Shops</Tab>
+        </TabList>
+
+        <TabPanels>
+          <TabPanel>
+            <p>one!</p>
+          </TabPanel>
+          <TabPanel>
+            {shopsInfo &&
+              shopsInfo.map((shop: any, key: any) => {
+                return (
+                  <ShopCard
+                    key={key}
+                    display_name={shop.display_name}
+                    handle_name={shop.handle_name}
+                    icon={shop.icon}
+                    concept={shop.concept}
+                  />
+                )
+              })}
+            {!shopsInfo.length && (
+              <>
+                <Text>検索結果：なし</Text>
+              </>
+            )}
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
+    </Box>
   )
 }
 
