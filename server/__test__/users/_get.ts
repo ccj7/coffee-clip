5650acc35e2848063d41b09d6e6fd746de0a36f3
Maho-Miyazawa <61673527+Maho-Miyazawa@users.noreply.github.com>
2022-01-30 22:58:56 +0900

    feat: usersのGET testを追加

---
+++ server/__test__/users/_get.ts
@@ -0,0 +1,62 @@
+import chai, { expect } from 'chai'
+import chaiHttp from 'chai-http'
+import connectToDB from '../../db-connection'
+import server from '../../app'
+import runSeedShop from '../../shop/seed'
+import mongoose from 'mongoose'
+import usersData from '../../user/users.json'
+
+chai.use(chaiHttp)
+
+describe('Users Get Request Tests', () => {
+    before(async () => {
+        await connectToDB()
+        await runSeedShop()
+    })
+
+    after(async () => {
+        mongoose.connection.close()
+    })
+
+    describe('GET Users', () => {
+        it('GET /api/users', async () => {
+            const res = await chai.request(server).get('/api/users')
+            const users = usersData.users
+            expect(res.body.length).to.equal(users.length)
+        })
+
+        it('GET /api/users/search', async () => {
+            const res = await chai
+                .request(server)
+                .get('/api/users/search?keyword=ma')
+
+            expect(res.body.users.length).to.equal(2)
+            expect(res.body.shops.length).to.equal(2)
+        })
+
+        it('GET /api/users/search 2', async () => {
+            const res = await chai
+                .request(server)
+                .get('/api/users/search?keyword=foo')
+            const expectedData = {
+                users: [],
+                shops: [],
+            }
+            expect(res.body).to.deep.equal(expectedData)
+        })
+
+        it('GET /api/users/:authId', async () => {
+            const res = await chai
+                .request(server)
+                .get('/api/users/h1ERSr4qUNUoviCQlzZ0648p1cA2')
+            const userHandleName = usersData.users[0].handle_name
+            const userFollowerHandleName =
+                usersData.users[0].follower_handle_names
+
+            expect(res.body.handle_name).to.deep.equal(userHandleName)
+            expect(res.body.follower_handle_names).to.deep.equal(
+                userFollowerHandleName
+            )
+        })
+    })
+})
