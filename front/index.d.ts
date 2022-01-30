6bf88308b582586f6e085d07fc478b7cac487604
naonmr <86028587+naonmr@users.noreply.github.com>
2022-01-30 11:22:08 +0900

    fix: fix-shopの型定義 修正

--- front/index.d.ts
+++ front/index.d.ts
@@ -7,3 +7,29 @@ type GetAccessControl = () =>
 type WithGetAccessControl<P> = P & {
   getAccessControl?: GetAccessControl
 }
+
+type ShopData = {
+  auth_id: string
+  handle_name: string
+  display_name: string
+  icon: string
+  address: string
+  map_url: string
+  hp_url: string
+  instagram_url: string
+  opening_hours: string
+  regular_day_off: string
+  concept: string
+  recommendation: {
+    title: string
+    description: string
+    image: string
+  }
+  selling_point: {
+    text: string
+    image: string
+  }
+  follower_handle_name: string[]
+  __v: string
+  _id: string
+}
--- front/pages/shop/fix-shopinfo/[handle_name]/index.tsx
+++ front/pages/shop/fix-shopinfo/[handle_name]/index.tsx
@@ -11,64 +11,13 @@ import axios from 'axios'
 import { useRouter } from 'next/router'
 import ImageUpload from '../../../../components/ImageUpload'
 
-type Data = {
-  auth_id: string
-  handle_name: string
-  display_name: string
-  icon: string
-  address: string
-  map_url: string
-  hp_url: string
-  instagram_url: string
-  opening_hours: string
-  regular_day_off: string
-  concept: string
-  recommendation: {
-    title: string
-    description: string
-    image: string
-  }
-  selling_point: {
-    text: string
-    image: string
-  }
-  follower_handle_name: string[]
-  __v: string
-  _id: string
-}
-
 const FixShopInfo: WithGetAccessControl<VFC> = () => {
   const { currentUser } = useAuthContext()
 
   const router = useRouter()
   const { handle_name } = router.query
 
-  const dammy = {
-    auth_id: '',
-    handle_name: '',
-    display_name: '',
-    icon: '',
-    address: '',
-    map_url: '',
-    hp_url: '',
-    instagram_url: '',
-    opening_hours: '',
-    regular_day_off: '',
-    concept: '',
-    recommendation: {
-      title: '',
-      description: '',
-      image: '',
-    },
-    selling_point: {
-      text: '',
-      image: '',
-    },
-    follower_handle_name: [''],
-    __v: '',
-    _id: '',
-  }
-  const [shopInfo, setShopInfo] = useState<Data>(dammy)
+  const [shopInfo, setShopInfo] = useState<ShopData | null>(null)
 
   const methods = useForm()
 
@@ -98,87 +47,89 @@ const FixShopInfo: WithGetAccessControl<VFC> = () => {
         <meta name="shopTopPage" content="shop Top Page" />
       </Head>
       <Header />
-      <FormProvider {...methods}>
-        <form onSubmit={methods.handleSubmit(onSubmit)}>
-          <InputForm
-            thema="handle_name"
-            text="shop ID"
-            defaultValue={shopInfo.handle_name}
-          />
-          <InputForm
-            thema="display_name"
-            text="お店の名前"
-            defaultValue={shopInfo.display_name}
-          />
-          <ImageUpload size="sm" thema="icon" text="アイコン画像" />
-          <InputForm
-            thema="address"
-            text="住所"
-            defaultValue={shopInfo.address}
-          />
-          <InputForm
-            thema="map_url"
-            text="googlemap URL"
-            defaultValue={shopInfo.map_url}
-          />
-          <InputForm
-            thema="hp_url"
-            text="HP URL"
-            defaultValue={shopInfo.hp_url}
-          />
-          <InputForm
-            thema="instagram_url"
-            text="instagram URL"
-            defaultValue={shopInfo.instagram_url}
-          />
-          <InputForm
-            thema="opening_hours"
-            text="営業時間"
-            defaultValue={shopInfo.opening_hours}
-          />
-          <InputForm
-            thema="regular_day_off"
-            text="定休日"
-            defaultValue={shopInfo.regular_day_off}
-          />
-          <InputForm
-            thema="concept"
-            text="お店のプロフィール文"
-            defaultValue={shopInfo.concept}
-          />
-          <InputForm
-            thema="recommendation.title"
-            text="おすすめのコーヒー 名前"
-            defaultValue={shopInfo.recommendation.title}
-          />
-          <InputForm
-            thema="recommendation.description"
-            text="おすすめのコーヒー 紹介文"
-            defaultValue={shopInfo.recommendation.description}
-          />
+      {shopInfo && (
+        <FormProvider {...methods}>
+          <form onSubmit={methods.handleSubmit(onSubmit)}>
+            <InputForm
+              thema="handle_name"
+              text="shop ID"
+              defaultValue={shopInfo.handle_name}
+            />
+            <InputForm
+              thema="display_name"
+              text="お店の名前"
+              defaultValue={shopInfo.display_name}
+            />
+            <ImageUpload size="sm" thema="icon" text="アイコン画像" />
+            <InputForm
+              thema="address"
+              text="住所"
+              defaultValue={shopInfo.address}
+            />
+            <InputForm
+              thema="map_url"
+              text="googlemap URL"
+              defaultValue={shopInfo.map_url}
+            />
+            <InputForm
+              thema="hp_url"
+              text="HP URL"
+              defaultValue={shopInfo.hp_url}
+            />
+            <InputForm
+              thema="instagram_url"
+              text="instagram URL"
+              defaultValue={shopInfo.instagram_url}
+            />
+            <InputForm
+              thema="opening_hours"
+              text="営業時間"
+              defaultValue={shopInfo.opening_hours}
+            />
+            <InputForm
+              thema="regular_day_off"
+              text="定休日"
+              defaultValue={shopInfo.regular_day_off}
+            />
+            <InputForm
+              thema="concept"
+              text="お店のプロフィール文"
+              defaultValue={shopInfo.concept}
+            />
+            <InputForm
+              thema="recommendation.title"
+              text="おすすめのコーヒー 名前"
+              defaultValue={shopInfo.recommendation.title}
+            />
+            <InputForm
+              thema="recommendation.description"
+              text="おすすめのコーヒー 紹介文"
+              defaultValue={shopInfo.recommendation.description}
+            />
 
-          <ImageUpload
-            size="sm"
-            thema="recommendation.image"
-            text="おすすめのコーヒー 写真"
-          />
+            <ImageUpload
+              size="sm"
+              thema="recommendation.image"
+              text="おすすめのコーヒー 写真"
+            />
 
-          <InputForm
-            thema="selling_point.text"
-            text="お店の魅力"
-            defaultValue={shopInfo.recommendation.image}
-          />
+            <InputForm
+              thema="selling_point.text"
+              text="お店の魅力"
+              defaultValue={shopInfo.recommendation.image}
+            />
 
-          <ImageUpload
-            size="sm"
-            thema="selling_point.image"
-            text="お店の魅力　写真"
-          />
-          <Button mt={4} type="submit">
-            Submit
-          </Button>
-        </form>
-      </FormProvider>
+            <ImageUpload
+              size="sm"
+              thema="selling_point.image"
+              text="お店の魅力　写真"
+            />
+            <Button mt={4} type="submit">
+              Submit
+            </Button>
+          </form>
+        </FormProvider>
+      )}
     </div>
   )
 }
