bff54ed2c8543ef8671ff12e51ab7fb9e169da6b
naonmr <86028587+naonmr@users.noreply.github.com>
2022-01-30 11:35:45 +0900

    fix: 不要なconsole.logなどを削除 at user-shopTopPage

--- front/pages/user/shop-top-page/[handle_name]/index.tsx
+++ front/pages/user/shop-top-page/[handle_name]/index.tsx
@@ -1,34 +1,15 @@
-import {
-  Box,
-  Center,
-  HStack,
-  Icon,
-  Spacer,
-  Stack,
-  Text,
-  Tabs,
-  TabList,
-  TabPanels,
-  Tab,
-  TabPanel,
-} from '@chakra-ui/react'
-import { AiOutlineInstagram } from 'react-icons/ai'
+import { VFC } from 'react'
 import Head from 'next/head'
-import { useContext, useEffect, useState, VFC } from 'react'
-import PrimaryButton from '../../../../components/Button'
-import Profile from '../../../../components/Profile'
-
-import Link from 'next/link'
 import { useRouter } from 'next/router'
-import axios from 'axios'
-import PostImage from '../../../../components/Image'
-import { AuthContext, useAuthContext } from '../../../../auth/AuthContext'
+
+import { useAuthContext } from '../../../../auth/AuthContext'
 import { isLoggedIn } from '../../../../util'
+
 import UserHeader from '../../../../components/user/UserHeader'
 import ShopTopPage from '../../../../components/ShopTopPage'
 
 const ShopTopPageOfUser: WithGetAccessControl<VFC> = () => {
-  const { currentUser } = useContext(AuthContext)
+  const { currentUser } = useAuthContext()
 
   const router = useRouter()
   const { handle_name } = router.query
