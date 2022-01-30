7420dd274c25bea7a37d30b3cf5936b0dae9c3f0
naonmr <86028587+naonmr@users.noreply.github.com>
2022-01-30 11:24:28 +0900

    fix: 不要なimportを削除

--- front/pages/shop/top-page/[handle_name]/index.tsx
+++ front/pages/shop/top-page/[handle_name]/index.tsx
@@ -1,30 +1,11 @@
-import {
-  Box,
-  Center,
-  HStack,
-  Icon,
-  Spacer,
-  Stack,
-  Text,
-  Tabs,
-  TabList,
-  TabPanels,
-  Tab,
-  TabPanel,
-} from '@chakra-ui/react'
-import { AiOutlineInstagram } from 'react-icons/ai'
+import { useContext, VFC } from 'react'
 import Head from 'next/head'
-import { useContext, useEffect, useState, VFC } from 'react'
-import PrimaryButton from '../../../../components/Button'
-import Profile from '../../../../components/Profile'
-
-import Header from '../../../../components/shop/Header'
-import Link from 'next/link'
 import { useRouter } from 'next/router'
-import axios from 'axios'
-import PostImage from '../../../../components/Image'
+
 import { AuthContext, useAuthContext } from '../../../../auth/AuthContext'
 import { isLoggedIn } from '../../../../util'
+
+import Header from '../../../../components/shop/Header'
 import ShopTopPage from '../../../../components/ShopTopPage'
 
 const ShopTopPageOfShop: WithGetAccessControl<VFC> = () => {
