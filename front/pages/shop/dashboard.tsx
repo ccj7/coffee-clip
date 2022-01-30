4d69464bc6cc71ff3856f31d67b1681c536e9d95
naonmr <86028587+naonmr@users.noreply.github.com>
2022-01-30 11:27:14 +0900

    fix: 型定義と不要なconsole.logなどを削除

--- front/pages/shop/dashboard.tsx
+++ front/pages/shop/dashboard.tsx
@@ -1,61 +1,34 @@
+import { useEffect, useState, VFC } from 'react'
+import { useRouter } from 'next/router'
 import Head from 'next/head'
+import axios from 'axios'
+
+import { useAuthContext } from '../../auth/AuthContext'
+import { isLoggedIn } from '../../util'
 
 import Header from '../../components/shop/Header'
 import Profile from '../../components/Profile'
 import PrimaryButton from '../../components/Button'
-import { useEffect, useRef, useState, VFC } from 'react'
-import axios from 'axios'
+
 import { Spacer } from '@chakra-ui/react'
-import Link from 'next/link'
-import { useRouter } from 'next/router'
-import { useAuthContext } from '../../auth/AuthContext'
-import { isLoggedIn } from '../../util'
 
 const DashBoard: WithGetAccessControl<VFC> = () => {
   const { currentUser } = useAuthContext()
 
   const router = useRouter()
-  // TODO: dammyデータ削除 & Interfaceをサーバー側のスキーマから流用して作成
-  const dammy = {
-    auth_id: '',
-    handle_name: '',
-    display_name: '',
-    icon: '',
-    address: '',
-    map_url: '',
-    hp_url: '',
-    instagram_url: '',
-    opening_hours: '',
-    regular_day_off: '',
-    concept: '',
-    recommendation: {
-      title: '',
-      description: '',
-      image: '',
-    },
-    selling_point: {
-      text: '',
-      image: '',
-    },
-    follower_handle_name: [''],
-  }
 
-  const [shopInfo, setShopInfo] = useState<any>(dammy)
+  const [shopInfo, setShopInfo] = useState<ShopData | any>(null)
 
   //　user情報を取得
   useEffect(() => {
-    console.log(currentUser)
     const getShop = async (authId: string) => {
       const res: any = await axios.get(`/api/shops/${authId}`)
-      console.log(res.data)
       setShopInfo(res.data)
     }
 
     if (currentUser) {
       getShop(currentUser)
     }
-
-    console.log(shopInfo)
   }, [currentUser])
 
   return (
@@ -65,12 +38,13 @@ const DashBoard: WithGetAccessControl<VFC> = () => {
         <meta name="dashboard" content="ダッシュボード" />
       </Head>
       <Header />
-      {console.log(shopInfo.handle_name)}
-      <Profile
-        display_name={shopInfo.display_name}
-        handle_name={shopInfo.handle_name}
-        icon={shopInfo.icon}
-      />
+      {shopInfo && (
+        <Profile
+          display_name={shopInfo.display_name}
+          handle_name={shopInfo.handle_name}
+          icon={shopInfo.icon}
+        />
+      )}
       <PrimaryButton
         text="編集"
         onclick={() =>
