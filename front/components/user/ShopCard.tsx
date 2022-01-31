46753aff25c305485b46608e5f4d79c9e7c5d355
mei-omomo <l.3.llxl@gmail.com>
2022-01-31 19:08:22 +0900

    css: shoplistに取り掛かり

--- front/components/user/ShopCard.tsx
+++ front/components/user/ShopCard.tsx
@@ -7,17 +7,24 @@ function ShopCard(props: any) {
   return (
     // TODO: 本番はhandle_nameを使ったLink先
     <Link href={{ pathname: `/user/shop-top-page/${handle_name}` }}>
-      <Box
-        w={600}
+      <Box 
+        w={{ base:'350px', md:'500px' }}
         m={10}
-        border="2px"
-        borderColor="gray.200"
+        backgroundColor={"brand.sub1"}
+        border="0.2px"
+        color='gray.100'
+        borderRadius='24px'
+        borderColor="gray.100"
         textAlign="center"
+        padding='20px'
+        boxShadow='0px 2px 6px rgba(0, 0, 0, 0.3)'
+        transition='0.3s'
+        _hover={{transform: 'translate3d(1px, 3px, 1px)'}}
       >
-        <Heading size="lg">{display_name}</Heading>
-        <Text>{handle_name}</Text>
+        <Heading size="md" mb='8px'>{display_name}</Heading>
+        <Text fontSize='16px' mb='20px'>@{handle_name}</Text>
         <Center>
-          <Image w={300} src={icon} />
+          <Image w={{ base:'200px', md:'300px'}} mb='20px' src={icon} />
         </Center>
         <Text>{concept}</Text>
       </Box>
