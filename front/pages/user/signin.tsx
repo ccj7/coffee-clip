5fcf640d308178a49e58a90d52fc6554c1473e80
Maho Miyata <k.paddy123+github@gmail.com>
2022-02-01 17:13:25 +0900

    fix: CSS追加

--- front/pages/user/signin.tsx
+++ front/pages/user/signin.tsx
@@ -8,10 +8,12 @@ import firebase from '../../auth/firebaseConfig'
 
 import { isLoggedIn } from '../../util'
 
-import Header from '../../components/shop/Header'
+import Header from '../../components/Header'
 import InputForm from '../../components/InputForm'
 
-import { Button } from '@chakra-ui/react'
+import { Box, Button, Heading, Center, HStack } from '@chakra-ui/react'
+import { FiCoffee } from 'react-icons/fi'
+import Link from 'next/link'
 
 const Signin: WithGetAccessControl<VFC> = () => {
   const methods = useForm()
@@ -32,23 +34,51 @@ const Signin: WithGetAccessControl<VFC> = () => {
       })
   }
   return (
-    <div>
+    <Box
+      background="#988d83"
+      backgroundImage="linear-gradient(62deg, #988d83 0%, #f7dcae 100%)"
+      pb='400px'>
       <Head>
         <title>Sign-In</title>
         <meta name="Sign-In" content="ユーザーサインイン" />
       </Head>
       <Header />
-
-      <FormProvider {...methods}>
-        <form onSubmit={methods.handleSubmit(onSubmit)}>
-          <InputForm thema="email" text="メールアドレス" />
-          <InputForm thema="password" text="パスワード" />
-          <Button mt={4} type="submit">
-            Submit
-          </Button>
-        </form>
-      </FormProvider>
-    </div>
+      <Box
+        w={{ base: '80%', md: '65%' }}
+        ml="auto" mr="auto">
+        <Box
+            my={12} p={8} mt='70px'
+            borderRadius='16px'
+            backgroundColor='orange.50'
+            boxShadow='0px 2px 6px rgba(0, 0, 0, 0.3)'>
+            <Center h='50px' mb='25px'>
+              <HStack color='brand.color1'>
+                <FiCoffee /> 
+                <Heading as='h2' size='md'>Welcome Back!</Heading>
+              </HStack>
+            </Center>
+        <FormProvider {...methods}>
+          <form onSubmit={methods.handleSubmit(onSubmit)}>
+            <InputForm thema="email" text="メールアドレス" />
+            <InputForm thema="password" text="パスワード" />
+            <Center mt='10px'>
+              <Button
+                backgroundColor='brand.color2'
+                color='white'
+                _hover={{background: '#c58573'}}
+                mt={4}
+                type="submit">
+                ログイン
+              </Button>
+            </Center>
+          </form>
+        </FormProvider>
+        <Center mt='30px' fontSize='sm'>
+          <Link href='/user/signup'>COFFEE CLIPのアカウントを作成する場合はこちら</Link>
+        </Center>
+        </Box>
+      </Box>
+    </Box>
   )
 }
 
