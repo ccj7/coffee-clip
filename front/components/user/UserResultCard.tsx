40f9773946391cfe578db0953aa07adf07f705c7
Maho Miyata <k.paddy123+github@gmail.com>
2022-02-01 20:16:21 +0900

    fix: CSS追加

--- front/components/user/UserResultCard.tsx
+++ front/components/user/UserResultCard.tsx
@@ -1,21 +1,19 @@
-import { Avatar, Stack, HStack, Heading, Box } from '@chakra-ui/react'
+import { Avatar, Stack, HStack, Heading, Box, Text } from '@chakra-ui/react'
 import Link from 'next/link'
 
 // TODO propsの型を変換する
 function UserResultCard(props: any) {
   const { display_name, handle_name, icon } = props
   return (
-    <>
+    <Box p='15px' mb='5px'>
       <Link href={{ pathname: `/user/topPage/${handle_name}` }}>
-        <HStack>
-          <Avatar size="lg" src={icon} alt="icon" />{' '}
-          <Stack>
-            <Heading size="lg">{display_name}</Heading>
-            <Box>@{handle_name}</Box>
-          </Stack>
+        <HStack style={{cursor: 'pointer'}}>
+          <Avatar size="lg" src={icon} alt="icon" mr='8px'/>
+          <Heading size="lg">{display_name}</Heading>
+          <Text fontSize='sm'>@{handle_name}</Text>
         </HStack>
       </Link>
-    </>
+    </Box>
   )
 }
 
