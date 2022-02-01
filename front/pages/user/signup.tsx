19e59c9de301a0293b1667ee2a5ecfbd4525d4ae
Maho Miyata <k.paddy123+github@gmail.com>
2022-02-01 15:46:59 +0900

    fix: SignUp画面CSS追加

--- front/pages/user/signup.tsx
+++ front/pages/user/signup.tsx
@@ -6,14 +6,15 @@ import axios from 'axios'
 
 import { createUserWithEmailAndPassword, getAuth } from 'firebase/auth'
 import firebase from '../../auth/firebaseConfig'
-import { useAuthContext } from '../../auth/AuthContext'
 import { isLoggedIn } from '../../util'
 
-import Header from '../../components/shop/Header'
+import Header from '../../components/Header'
 import InputForm from '../../components/InputForm'
 import ImageUpload from '../../components/ImageUpload'
 
-import { Button } from '@chakra-ui/react'
+import { Box, Button, Heading, Center, HStack } from '@chakra-ui/react'
+import { FiCoffee } from 'react-icons/fi'
+import Link from 'next/link'
 
 const Signup: WithGetAccessControl<VFC> = () => {
   const methods = useForm()
@@ -56,25 +57,53 @@ const Signup: WithGetAccessControl<VFC> = () => {
       })
   }
   return (
-    <div>
+    <Box
+      background="#988d83"
+      backgroundImage="linear-gradient(62deg, #988d83 0%, #f7dcae 100%)"
+      pb='20px'>
       <Head>
         <title>ユーザー Sign-Up</title>
         <meta name="Sign-Up" content="ユーザー サインアップ" />
       </Head>
       <Header />
-      <FormProvider {...methods}>
-        <form onSubmit={methods.handleSubmit(onSubmit)}>
-          <InputForm thema="display_name" text="ユーザーネーム" />
-          <InputForm thema="handle_name" text="ユーザーID" />
-          <InputForm thema="email" text="メールアドレス" />
-          <InputForm thema="password" text="パスワード" />
-          <ImageUpload size="sm" thema="icon" text="アイコン画像" />
-          <Button mt={4} type="submit">
-            Submit
-          </Button>
-        </form>
-      </FormProvider>
-    </div>
+      <Box
+        w={{ base: '80%', md: '65%' }}
+        ml="auto" mr="auto">
+        <Box
+          my={12} p={8}
+          borderRadius='16px'
+          backgroundColor='orange.50'
+          boxShadow='0px 2px 6px rgba(0, 0, 0, 0.3)'>
+          <Center h='50px' mb='25px'>
+            <HStack color='brand.color1'>
+              <FiCoffee /> 
+              <Heading as='h2' size='md'>Welcome to COFFEE CLIP</Heading>
+            </HStack>
+          </Center>
+          <FormProvider {...methods}>
+            <form onSubmit={methods.handleSubmit(onSubmit)}>
+              <InputForm thema="display_name" text="ユーザーネーム" />
+              <InputForm thema="handle_name" text="ユーザーID" />
+              <InputForm thema="email" text="メールアドレス" />
+              <InputForm thema="password" text="パスワード" />
+              <ImageUpload size="sm" thema="icon" text="アイコン画像" />
+              <Center mt='10px'>
+                <Button
+                  backgroundColor='brand.color2'
+                  color='white'
+                  mt={4}
+                  type="submit">
+                  アカウントを作成
+                </Button>
+              </Center>
+            </form>
+          </FormProvider>
+          <Center mt='30px' fontSize='sm'>
+            <Link href='/user/signin'>COFFEE CLIPのアカウントをお持ちですか？ログインはこちら</Link>
+          </Center>
+        </Box>
+      </Box>
+    </Box>
   )
 }
 
