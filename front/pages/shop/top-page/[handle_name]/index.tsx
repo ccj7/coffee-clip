e7f9d5304afbda713d6298b27f6010207d2d37c8
Maho Miyata <k.paddy123+github@gmail.com>
2022-02-02 19:58:45 +0900

    fix: スマホ対応

--- front/pages/shop/top-page/[handle_name]/index.tsx
+++ front/pages/shop/top-page/[handle_name]/index.tsx
@@ -6,7 +6,7 @@ import { isLoggedIn } from '../../../../util'
 import Header from '../../../../components/shop/Header'
 import ShopTopPage from '../../../../components/ShopTopPage'
 import PrimaryButton from '../../../../components/Button'
-import { Box, Center, Heading, Spacer } from '@chakra-ui/react'
+import { Box, Center, Heading, Spacer, SimpleGrid } from '@chakra-ui/react'
 
 const ShopTopPageOfShop: WithGetAccessControl<VFC> = () => {
   const router = useRouter()
@@ -21,19 +21,20 @@ const ShopTopPageOfShop: WithGetAccessControl<VFC> = () => {
       <Header />
       <Box backgroundColor="brand.color1" >
         <Center
-          height="70px"
+          height={{base: "100px", md: "70px"}}
           color="white"
           w={{ base: '80%', md: '65%' }}
           ml="auto"
           mr="auto">
-          <Heading size="md">店舗向けのプレビューページです</Heading>
-          <Spacer></Spacer>
-          <PrimaryButton
-            text={'ダッシュボードに戻る'}
-            onclick={() => {
-              router.push('/shop/dashboard')
-            }}
-          />
+            <SimpleGrid columns={{md:2}} spacing={{md: '50px'}}>
+              <Heading size="md" mb="5px" mt="8px">店舗向けのプレビューページ</Heading>
+              <PrimaryButton
+                text={'ダッシュボードに戻る'}
+                onclick={() => {
+                  router.push('/shop/dashboard')
+                }}
+              />
+            </SimpleGrid>
         </Center>
       </Box>
       <ShopTopPage handle_name={handle_name} isUser={false} />
