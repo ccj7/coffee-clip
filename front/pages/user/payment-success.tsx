9216271c03d6aeadb0384b9914a5f0cb45b2eabb
Maho Miyata <k.paddy123+github@gmail.com>
2022-02-03 18:21:42 +0900

    fix: CSS追加 & 不要な関数の削除

--- front/pages/user/payment-success.tsx
+++ front/pages/user/payment-success.tsx
@@ -1,56 +1,20 @@
-import { VFC, useState } from 'react'
+import { VFC } from 'react'
 import Head from 'next/head'
-import axios from 'axios'
-
-import { useAuthContext } from '../../auth/AuthContext'
 import { isLoggedIn } from '../../util'
 
 import UserHeader from '../../components/user/UserHeader'
-import InputForm from '../../components/InputForm'
-import ImageUpload from '../../components/ImageUpload'
-import Message from '../../components/Message'
-import { useForm, FormProvider } from 'react-hook-form'
-import { Heading, Box, Button } from '@chakra-ui/react'
+import { Box } from '@chakra-ui/react'
+import BoxMessage from '../../components/BoxMessage'
 
 const PatmentSuccess: WithGetAccessControl<VFC> = () => {
-  const { currentUser } = useAuthContext()
-
-  const methods = useForm()
-  const [message, setMessage] = useState<string>()
-
-  const postUser = async (userNewReview: Review, authId: string) => {
-    axios
-      .post(`/api/users/${authId}/reviews`, userNewReview, {
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      })
-      .then((res) => {
-        if (res.status === 200) {
-          setMessage('保存しました')
-        }
-      })
-  }
-
-  const onSubmit = (userNewReview: Review) => {
-    if (currentUser) {
-      postUser(userNewReview, currentUser)
-    }
-  }
 
   return (
     <Box>
       <Head>
-        <title>新規投稿</title>
-        <meta name="NewReview" content="プロフィール編集" />
+        <title>支払い完了</title>
       </Head>
       <UserHeader />
-      <Box w={{ base: '80%', md: '65%' }} my="0" mx="auto">
-        <Heading size="md" m={'16px'}>
-          支払いが完了しました。
-        </Heading>
-        {message && <Message message={message} />}
-      </Box>
+      <BoxMessage heading="支払いが完了しました。" buttonLabel="オンラインショップへ戻る" path="/user/onlineShop" />
     </Box>
   )
 }
