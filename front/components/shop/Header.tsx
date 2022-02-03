8d2ad995c4352ec35f1c7ed3cc896ccdd193d420
Maho Miyata <k.paddy123+github@gmail.com>
2022-02-03 20:36:59 +0900

    fix: COFFEE CLIPの文字をホバーしたときの挙動を修正

--- front/components/shop/Header.tsx
+++ front/components/shop/Header.tsx
@@ -8,11 +8,11 @@ import {
   Spacer,
   Text,
   Box,
+  Link
 } from '@chakra-ui/react'
 import { HamburgerIcon, NotAllowedIcon } from '@chakra-ui/icons'
 
 import { getAuth, signOut } from 'firebase/auth'
-import Link from 'next/link'
 import firebase from '../../auth/firebaseConfig'
 import { useRouter } from 'next/router'
 
@@ -35,7 +35,7 @@ function Header() {
     <>
       <Box top={0} position="sticky" zIndex={'sticky'}>
         <HStack p="2" bg="brand.color1" color="brand.color6">
-          <Link href="/shop/dashboard">
+          <Link href="/shop/dashboard" _hover={{textDecoration: "none"}}>
             <Text ml="10px" fontSize={{ base: 'lg', md: '3xl' }}>
               COFFEE CLIP
             </Text>
