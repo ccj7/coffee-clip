2907b0ffada0c78477e89271722f702275d4bf6d
Motoki-tech <>
2022-02-03 12:00:17 +0900

    リファクタリング

--- server/stripe/route.ts
+++ server/stripe/route.ts
@@ -3,28 +3,9 @@ import { Request, Response } from 'express'
 require('dotenv').config()
 
 const routes = Router()
-
-// app.use(express.static('public'))
-
-const YOUR_DOMAIN =
-    'http://ec2-18-183-145-70.ap-northeast-1.compute.amazonaws.com/'
-
 const Stripe = require('stripe')(`${process.env.STRIPE_SECRET_KEY}`)
 
-routes.post('/api/test', async (req: Request, res: Response) => {
-    const account = await Stripe.accounts.create({ type: 'standard' })
-
-    const accountLink = await Stripe.accountLinks.create({
-        account: account.id,
-        refresh_url: `${YOUR_DOMAIN}`,
-        return_url: `${YOUR_DOMAIN}`,
-        type: 'account_onboarding',
-    })
-
-    res.json(accountLink.url)
-})
-
-routes.post('/checkout_sessions', async (req, res) => {
+routes.post('/checkout_sessions', async (req: Request, res: Response) => {
     try {
         const session = await Stripe.checkout.sessions.create({
             line_items: [
