2dd055f1769b9d7e0c6e8b70dc257b7ed5a69016
Motoki-tech <>
2022-02-03 14:35:36 +0900

    コメントアウト削除

--- server/app.ts
+++ server/app.ts
@@ -8,16 +8,6 @@ const app: express.Express = express()
 app.use(express.json({ limit: '10mb' }))
 app.use(express.urlencoded({ limit: '10mb' }))
 
-// ToDo: CROS対応 要検討（以下は本番環境ではだめ絶対）
-// app.use(
-//   (req: express.Request, res: express.Response, next: express.NextFunction) => {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Methods", "*");
-//     res.header("Access-Control-Allow-Headers", "*");
-//     next();
-//   }
-// );
-
 app.use('/api/shops', shopRoute)
 app.use('/api/users', userRoute)
 app.use('/api/stripe', stripeRoute)
