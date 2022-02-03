3a50d5b59593d1f62411cec03084433e3df1d85d
Maho-Miyazawa <61673527+Maho-Miyazawa@users.noreply.github.com>
2022-02-03 15:36:17 +0900

    feat: GET api/shops/handle/:handleNameを追加

--- server/shop/route.ts
+++ server/shop/route.ts
@@ -1,10 +1,18 @@
 import { Router } from 'express'
-import { getShops, getShop, putShop, postShop, getShopFromUser } from './controller'
+import {
+    getShops,
+    getShop,
+    getShopHandleNameCheck,
+    putShop,
+    postShop,
+    getShopFromUser,
+} from './controller'
 
 const routes = Router()
 
 routes.get('/', getShops)
 routes.get('/details/:handleName', getShop)
+routes.get('/handle/:handleName', getShopHandleNameCheck)
 routes.get('/:authId', getShop)
 routes.get('/:authId/:handleName', getShopFromUser)
 
