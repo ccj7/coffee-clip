4368611712acdeb8b07514d849117e9a0f3bd826
Maho Miyata <k.paddy123+github@gmail.com>
2022-02-03 19:17:52 +0900

    fix: タブ削除 #301

--- front/components/ShopTopPage.tsx
+++ front/components/ShopTopPage.tsx
@@ -3,17 +3,9 @@ import {
   Center,
   HStack,
   Icon,
-  Spacer,
-  Stack,
   Text,
-  Tabs,
-  TabList,
-  TabPanels,
-  Tab,
-  TabPanel,
   Link,
   SimpleGrid,
-  Flex
 } from '@chakra-ui/react'
 import { AiOutlineInstagram } from 'react-icons/ai'
 import { useContext, useEffect, useState } from 'react'
@@ -24,6 +16,7 @@ import axios from 'axios'
 import PostImage from './Image'
 import { AuthContext } from '../auth/AuthContext'
 import { shopInitialData } from '../initial_data/shopInitialDdata'
+import { BiHome } from 'react-icons/bi';
 
 const ShopTopPage = (props: any) => {
   const { handle_name, isUser } = props
@@ -83,7 +76,7 @@ const ShopTopPage = (props: any) => {
      w="100%"
      h='300px'
      pt='50px'
-     background="#988d83 "
+     background="#988d83"
      backgroundImage="linear-gradient(62deg, #988d83  0%, #af6938 100%)"
     >
       <Box 
@@ -91,8 +84,8 @@ const ShopTopPage = (props: any) => {
         ml='auto' mr='auto' my='0'
         borderRadius="46px"
         backgroundColor='orange.50'
-        h='405px'
         pt='10px'
+        pb='40px'
         mb="45px">
       
         <Profile
@@ -100,6 +93,10 @@ const ShopTopPage = (props: any) => {
           handle_name={shopInfo.handle_name}
           icon={shopInfo.icon}
         />
+
+        <Center mb={{base: '30px', md: '40px'}} w={{base: '60%'}} ml='auto' mr='auto'>
+          <Text fontSize='sm' fontWeight='bold'>{shopInfo.concept}</Text>
+        </Center>
 
         {/* お気に入りの数 */}
         <HStack
@@ -125,8 +122,9 @@ const ShopTopPage = (props: any) => {
         </HStack>
 
         {/* お気に入り登録ボタン */}
+      </Box>
         <Center>
-        <Box mt='14px'>
+        <Box mt={{base: '-90px'}}>
           {isUser && (
             <PrimaryButton
               text={shopInfo.is_following ? 'お気に入りに登録済み' : 'お気に入りに登録する'}
@@ -140,7 +138,6 @@ const ShopTopPage = (props: any) => {
           )}
         </Box>
         </Center>
-      </Box>
 
       {/* お店の情報 */}
       <Box
@@ -151,17 +148,21 @@ const ShopTopPage = (props: any) => {
         backgroundColor='orange.50'
         borderRadius="40px"
         p="40px"
+        mb='30px'
       >
-        <SimpleGrid columns={{md:2}}>
-          <Box mb='26px' mt='6px'>
+        <SimpleGrid columns={{md:2}} spacingX="40px">
+          <Box mb='26px'>
             <Link href={shopInfo.map_url} isExternal>
-              住所：{shopInfo.address}
+              {shopInfo.address}
             </Link>
           </Box>
           <Box mb='26px'>
+          <HStack color="brand.color1">
+            <BiHome />
             <Link href={shopInfo.hp_url} isExternal>
-              HP：{shopInfo.hp_url}
+              {shopInfo.hp_url}
             </Link>
+          </HStack>
           </Box>
           <Box mb='26px'><Text>営業時間：{shopInfo.opening_hours}</Text></Box>
           <Box mb='26px'><Text>定休日：{shopInfo.regular_day_off}</Text></Box>
@@ -177,127 +178,73 @@ const ShopTopPage = (props: any) => {
         </SimpleGrid>
       </Box>
       
-      {/* リード */}
-      <Box 
-        w={{base: '80%', md: '65%'}} 
-        ml="auto" 
-        mr="auto"
-        my='80px'
-        mb='30px'
-      >
-        <Text fontSize='md' fontWeight='bold'>{shopInfo.concept}</Text>
-      </Box>
-      
-      {/* タブ */}
-      <Box>
-        <Tabs
-          isFitted
+      {/* お店の紹介 */}
+
+      <Box pb="100px">
+        <Box
           w={{base: '80%', md: '65%'}} 
           ml="auto" 
-          mr="auto"
-          pb='200px'
+          mr="auto" 
+          backgroundColor='orange.50'
+          borderRadius="40px"
+          p="30px"
         >
-
-        <TabList borderBottom='none'>
-          <Tab 
-            pt='13px' 
-            pb='13px'
-            fontWeight='bold'
-            borderBottom='2px solid'
-            borderColor='brand.color3'
-            _selected={{
-              color: 'white',
-              bg: 'brand.color3',
-              borderTopRadius: '16px',
-              boxShadow: '-1px -1px 6px rgba(0, 0, 0, 0.3)'
-            }}>
-            お店の紹介
-          </Tab>
-          <Tab
-            pt='13px' 
-            pb='13px'
-            fontWeight='bold'
-            borderBottom='2px solid'
-            borderColor='brand.color3'
-            _selected={{ 
-              color: 'white',
-              bg: 'brand.color3',
-              borderTopRadius: '16px',
-              boxShadow: '-1px -1px 6px rgba(0, 0, 0, 0.3)'
-            }}>
-            コーヒー豆購入
-          </Tab>
-        </TabList>
-
-        <TabPanels 
-          borderBottomRadius='16px'
-          backgroundColor='orange.50'
-          boxShadow='0px 2px 6px rgba(0, 0, 0, 0.3)'>
-
-          <TabPanel>
-            <Box
-              m='20px 20px 50px 20px'
-              columns={{md:2}}>
-              <Text 
-                fontSize='18px' 
-                fontWeight='bold'
-                mb='14px'>おすすめの一杯</Text>
-              <SimpleGrid columns={{md:2}}>
-                <Box>
-                {shopInfo.recommendation.image &&
-                  <PostImage
-                    src={shopInfo.recommendation.image}
-                    alt="おすすめの一杯の写真"
-                  />
-                }
-                </Box>
-                <Box>
-                  <Text
-                    fontSize='16px'>
-                      {shopInfo.recommendation.title}</Text>
-                  <Box 
-                    h='2.5px'
-                    backgroundColor='brand.color3'></Box>
-                  <Text 
-                    mt="10px"
-                    fontSize="14px">{shopInfo.recommendation.description}</Text>
-                </Box>
-              </SimpleGrid>
-            </Box>
-            
-            <Box
-              m='20px 20px 50px 20px'
-              columns={{md:2}}>
-              <Text 
-                fontSize='18px' 
-                fontWeight='bold'
-                mb='14px'>お店の魅力</Text>
-              <SimpleGrid columns={{md:2}}>
-                <Box>
-                {shopInfo.recommendation.image &&
-                  <PostImage
-                  src={shopInfo.selling_point.image}
-                  alt="お店の魅力を表す写真"
-                  />
-                }
-                </Box>
-                <Box>
-                  <Text 
-                    mt="10px"
-                    fontSize="14px">{shopInfo.selling_point.text}</Text>
-                </Box>
-              </SimpleGrid>
-            </Box>
-          </TabPanel>
-
-          <TabPanel
-          borderBottomRadius='16px'
-          backgroundColor='orange.50'
-          boxShadow='0px 2px 6px rgba(0, 0, 0, 0.3)'>
-            <p>コーヒー豆販売！</p>
-          </TabPanel>
-        </TabPanels>
-      </Tabs>
+        <Box>
+          <Box
+            m='20px 20px 50px 20px'
+            columns={{md:2}}>
+            <Text 
+              fontSize='18px' 
+              fontWeight='bold'
+              mb='14px'>おすすめの一杯</Text>
+            <SimpleGrid columns={{md:2}}>
+              <Box>
+              {shopInfo.recommendation.image &&
+                <PostImage
+                  src={shopInfo.recommendation.image}
+                  alt="おすすめの一杯の写真"
+                />
+              }
+              </Box>
+              <Box>
+                <Text
+                  fontSize='16px'>
+                    {shopInfo.recommendation.title}</Text>
+                <Box 
+                  h='2.5px'
+                  backgroundColor='brand.color3'></Box>
+                <Text 
+                  mt="10px"
+                  fontSize="14px">{shopInfo.recommendation.description}</Text>
+              </Box>
+            </SimpleGrid>
+          </Box>
+          
+          <Box
+            m='20px 20px 50px 20px'
+            columns={{md:2}}>
+            <Text 
+              fontSize='18px' 
+              fontWeight='bold'
+              mb='14px'>お店の魅力</Text>
+            <SimpleGrid columns={{md:2}}>
+              <Box>
+              {shopInfo.recommendation.image &&
+                <PostImage
+                src={shopInfo.selling_point.image}
+                alt="お店の魅力を表す写真"
+                />
+              }
+              </Box>
+              <Box>
+                <Text 
+                  mt="10px"
+                  fontSize="14px">{shopInfo.selling_point.text}</Text>
+              </Box>
+            </SimpleGrid>
+          </Box>
+        </Box>
+        </Box>
       </Box>
     </Box>
   )
