4ff71fd2e229921b648d843778f59dae3818e069
mei-omomo <l.3.llxl@gmail.com>
2022-02-04 20:42:13 +0900

    feat: logoとfaviconを追加

--- front/components/Header.tsx
+++ front/components/Header.tsx
@@ -5,16 +5,21 @@ import {
   Center,
   Link
 } from '@chakra-ui/react'
+import Image from 'next/image'
+import logo from '../img/logo.jpg'
 
 function Header() {
   return (
     <Box>
         <HStack p="2" bg="brand.color1" color="white">
           <Center w='100%'>
+          <Box w={{base:'54px', md:'38px'}} mr='4px'>
+            <Image src={logo} alt='COFFEE CLIP' />
+          </Box>
             <Link
               href="/" 
               _hover={{ borderBottom: 'none' }}>
-              <Text fontSize="3xl">COFFEE CLIP</Text>
+              <Text fontSize="3xl" ml='10px' >COFFEE CLIP</Text>
             </Link>
           </Center>
         </HStack>
--- front/components/shop/Header.tsx
+++ front/components/shop/Header.tsx
@@ -15,6 +15,8 @@ import { HamburgerIcon, NotAllowedIcon } from '@chakra-ui/icons'
 import { getAuth, signOut } from 'firebase/auth'
 import firebase from '../../auth/firebaseConfig'
 import { useRouter } from 'next/router'
+import Image from 'next/image'
+import logo from '../../img/logo.jpg'
 
 function Header() {
   const router = useRouter()
@@ -35,6 +37,9 @@ function Header() {
     <>
       <Box top={0} position="sticky" zIndex={'sticky'}>
         <HStack p="2" bg="brand.color1" color="brand.color6">
+          <Box w={{base:'54px', md:'38px'}} ml='4px'>
+            <Image src={logo} alt='COFFEE CLIP' />
+          </Box>
           <Link href="/shop/dashboard" _hover={{textDecoration: "none"}}>
             <Text ml="10px" fontSize={{ base: 'lg', md: '3xl' }}>
               COFFEE CLIP
