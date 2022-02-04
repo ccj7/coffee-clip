5ea9766d137add10696a72cc5351436fae10957b
naonmr <86028587+naonmr@users.noreply.github.com>
2022-02-04 14:14:09 +0900

    feat: 404ページにtopページに戻るボタン追加

--- front/pages/404.tsx
+++ front/pages/404.tsx
@@ -1,13 +1,25 @@
 import { Center, Heading } from '@chakra-ui/react'
+import { useRouter } from 'next/router'
 import { VFC } from 'react'
+import PrimaryButton from '../components/Button'
 
 const Custom404: WithGetAccessControl<VFC> = () => {
+  const router = useRouter()
+
   return (
     <>
       <Center mb="50px">
         <Heading size="md" mt="50px">
           404 - Page Not Found
         </Heading>
+      </Center>
+      <Center>
+        <PrimaryButton
+          text="topページに戻る"
+          onclick={() => {
+            router.push('/index')
+          }}
+        />
       </Center>
     </>
   )
